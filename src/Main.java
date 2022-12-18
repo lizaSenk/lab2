6e30975b61a7c945c1a4ce51d95356a5ab9891f3
lizaSenk <119893085+lizaSenk@users.noreply.github.com>
2022-12-18 14:12:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+// Конструируем экземпляр фрейма
+        JFrame frame = new JFrame();
+// Задаѐм реакцию на нажатие кнопки закрытия фрейма frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Показываем фрейм на экране
+        frame.setVisible(true);
+        frame.setSize(1200, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+}
